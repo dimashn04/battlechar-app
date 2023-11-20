4e883f588ead3d3083cb8494254ef947e88d565a
Dimas Herjunodarpito Notoprayitno <112958430+dimashn04@users.noreply.github.com>
2023-11-20 21:12:38 +0700

    Fixes @authentication:views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -29,3 +30,27 @@ def login(request):
             "status": False,
             "message": "Login gagal, periksa kembali email atau kata sandi."
         }, status=401)
+    
+@csrf_exempt
+def register(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({"status": False, "message": "Username already used."}, status=400)
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+
+    return JsonResponse({"username": user.username, "status": True, "message": "Register successful!"}, status=201)
+    
+    
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({"username": username, "status": True, "message": "Logout successful!"}, status=200)
+    except:
+        return JsonResponse({"status": False, "message": "Logout failed!"}, status=401)
